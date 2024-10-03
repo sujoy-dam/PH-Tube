5a7168a976939f1cf22d1a0d98ed020a16be839a
sujoy-dam <sujooy01@gmail.com>
2024-10-04 00:27:12 +0600

    blue batch symbol done

--- js/videos.js
+++ js/videos.js
@@ -11,17 +11,20 @@ const displayVideos = (videos) => {
         const card = document.createElement('div');
         card.classList="card card-compact bg-base-100 pb-5 shadow-xl"
         card.innerHTML =
-            `<figure>
-                    <img class="w-full h-[200px] rounded-lg"
+            `<figure class="h-[200px]">
+                    <img class="w-full h-full object-cover rounded-lg"
                         src=${video.thumbnail}
-                        alt="Shoes" />
+                        alt="video" />
                 </figure>
-                <div class="card-body">
+                <div class="px-2 pt-4">
                 <div class="flex gap-3">
-                    <img class="rounded-full w-10 h-10" src=${video.authors[0].profile_picture}>
+                    <img class="rounded-full w-10 h-10 object-cover" src=${video.authors[0].profile_picture}>
                     <div class="space-y-2">
                         <h2 class="card-title font-bold">${video.title}</h2>
-                        <p class="font-medium text-gray-400">${video.authors[0].profile_name}</p>
+                        <div class="flex items-center gap-2">
+                            <p class="font-medium text-gray-400">${video.authors[0].profile_name}</p>
+                            ${video.authors[0].verified === true?`<img class="w-5 h-5" src="https://img.icons8.com/?size=100&id=SRJUuaAShjVD&format=png&color=000000">`: ''}
+                        </div>
                         <p class="font-medium text-gray-400">${video.others.views} views</p>
                     </div>
                 </div>                 
