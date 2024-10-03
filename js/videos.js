a700c67f1179b3d9bfa108883e6128a870e038c5
sujoy-dam <sujooy01@gmail.com>
2024-10-04 02:24:39 +0600

    button functionality done

--- js/videos.js
+++ js/videos.js
@@ -1,20 +1,39 @@
+function calculateTime(time) {
+    const hour = parseInt(time / 3600);
+    const remainingSecond = time % 3600;
+    const minutes = parseInt(remainingSecond / 60);
+    const remainingRestSecond = minutes % 60;
+    return `${hour}hr ${minutes}m ${remainingRestSecond}s ago`
+}
+
+
 const loadVideos = () => {
     fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
         .then(res => res.json())
         .then(data => displayVideos(data.videos))
 }
+function selectCategoryVideos(id) {
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+        .then(res => res.json())
+        .then(data => displayVideos(data.category))
+        .catch((error) => console.log(error))
+}
 const displayVideos = (videos) => {
-    console.log(videos)
+    // console.log(videos)
     const videosContainer = document.getElementById('videos-container')
+    videosContainer.innerHTML = "";
     videos.forEach(video => {
-        console.log(video)
+        // console.log(video)
         const card = document.createElement('div');
-        card.classList="card card-compact bg-base-100 pb-5 shadow-xl"
+        card.classList = "card card-compact bg-base-100 pb-5 shadow-xl"
         card.innerHTML =
-            `<figure class="h-[200px]">
+            `<figure class="h-[200px] relative">
                     <img class="w-full h-full object-cover rounded-lg"
                         src=${video.thumbnail}
                         alt="video" />
+                        ${video.others.posted_date?.length === 0 ? "" : `<span class="text-gray-400 text-xs font-bold bg-black rounded-lg px-1 py-1 bottom-3 right-3 absolute">${calculateTime(video.others.posted_date)}</span>`
+            }
+                        
                 </figure>
                 <div class="px-2 pt-4">
                 <div class="flex gap-3">
@@ -23,13 +42,13 @@ const displayVideos = (videos) => {
                         <h2 class="card-title font-bold">${video.title}</h2>
                         <div class="flex items-center gap-2">
                             <p class="font-medium text-gray-400">${video.authors[0].profile_name}</p>
-                            ${video.authors[0].verified === true?`<img class="w-5 h-5" src="https://img.icons8.com/?size=100&id=SRJUuaAShjVD&format=png&color=000000">`: ''}
+                            ${video.authors[0].verified === true ? `<img class="w-5 h-5" src="https://img.icons8.com/?size=100&id=SRJUuaAShjVD&format=png&color=000000">` : ''}
                         </div>
                         <p class="font-medium text-gray-400">${video.others.views} views</p>
                     </div>
                 </div>                 
                 </div>`
-                videosContainer.append(card)
+        videosContainer.append(card)
     });
 
 }
